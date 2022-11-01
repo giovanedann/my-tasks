074bb55874a8fbf20b13daeb15dbfcb92b38b75f
Giovane <giovanedaniell@gmail.com>
2022-11-01 11:58:10 -0300

    style: add styles for Home component

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: '#f6f5fc',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#000',
+    fontSize: 40,
+  },
+})
+
+export default styles
