0374d5eaca353636ee21b984fe99ee601937b465
Giovane <giovanedaniell@gmail.com>
2022-11-01 15:24:33 -0300

    feat: refactor TaskList functions to use Context

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import {useTaskList} from '../../contexts/tasks'
 import Task from '../Task'
 import * as S from './styles'
 
-interface Props {
-  taskList: string[]
+interface RenderItemProps {
+  item: string
 }
 
-const TaskList = ({taskList}: Props) => {
+const TaskList = () => {
+  const {taskList, handleRemoveTask} = useTaskList()
+
   return (
     <S.Container>
       <S.Title>My tasks</S.Title>
       <S.List
         data={taskList}
         keyExtractor={(title: string, index: number) => title + index}
-        renderItem={({item}: {item: string}) => (
-          <Task title={item} handleDelete={() => {}} />
+        renderItem={({item}: RenderItemProps) => (
+          <Task title={item} handleDelete={() => handleRemoveTask(item)} />
         )}
       />
     </S.Container>
