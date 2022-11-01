c7451b6975543fbbc801bbf976683ccfa6f7f13c
Giovane <giovanedaniell@gmail.com>
2022-11-01 13:31:22 -0300

    style: add styles for containers of Home page

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,18 +4,37 @@ import styled from 'styled-components/native'
 export const SafeAreaView = styled.SafeAreaView`
   background-color: #f6f5fc;
   flex: 1;
+  align-items: center;
+  justify-content: center;
 `
 
 export const Wrapper = styled.View`
   background-color: #f6f5fc;
   flex: 1;
-  padding: 14px 20px;
+  width: 100%;
 `
 
-export const Title = styled.Text`
-  color: #000;
+export const Header = styled.View`
+  background-color: #4287f5;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100%;
+  padding: 10px;
+  height: 100px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+`
+
+export const HeaderTitle = styled.Text`
+  color: #fff;
   font-size: 24px;
   font-weight: bold;
+`
+
+export const TaskInputContainer = styled.View`
+  width: 100%;
+  height: 120px;
+  padding: 20px;
 `
 
 export const TaskInput = styled.TextInput`
@@ -25,4 +44,21 @@ export const TaskInput = styled.TextInput`
   font-size: 18px;
   padding: ${Platform.OS === 'android' ? '14px' : '10px'};
   border-radius: 8px;
+`
+
+export const AddTaskButton = styled.TouchableOpacity`
+  padding: 15px;
+  width: 100%;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 12px;
+  align-self: flex-end;
+  background-color: #1b70f7;
+`
+
+export const AddTaskButtonLabel = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #fff;
 `
