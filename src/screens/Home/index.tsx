00d229d6d77dd831794ad182a0565392842f72a3
Giovane <giovanedaniell@gmail.com>
2022-11-01 11:57:59 -0300

    feat(screens): create Home component

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+import styles from './styles'
+
+const Home = () => {
+  return (
+    <View style={styles.wrapper}>
+      <Text style={styles.title}>Hello world!</Text>
+    </View>
+  )
+}
+
+export default Home
