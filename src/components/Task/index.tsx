7e22de3555103f07807af4799b3ccc8b38b842c3
Giovane <giovanedaniell@gmail.com>
2022-11-01 17:09:50 -0300

    feat: add confirmation on task delete

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Alert} from 'react-native'
 import {useTaskList} from '../../contexts/tasks'
 import * as S from './styles'
 
@@ -9,12 +10,23 @@ interface Props {
 const Task = ({title}: Props) => {
   const {handleRemoveTask} = useTaskList()
 
+  const removeTask = (taskTitle: string) => {
+    Alert.alert('Warning', 'Are you sure? This action cannot be undone', [
+      {
+        text: 'Cancel',
+        onPress: () => {},
+      },
+      {
+        text: 'Confirm',
+        onPress: () => handleRemoveTask(taskTitle),
+      },
+    ])
+  }
+
   return (
     <S.Container>
       <S.Title>{title}</S.Title>
-      <S.DeleteButton
-        activeOpacity={0.6}
-        onPress={() => handleRemoveTask(title)}>
+      <S.DeleteButton activeOpacity={0.6} onPress={() => removeTask(title)}>
         <S.DeleteButtonLabel>Delete</S.DeleteButtonLabel>
       </S.DeleteButton>
     </S.Container>
