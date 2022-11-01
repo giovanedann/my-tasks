32c0a2973e50dda2564c778222b4a2b7062c09e5
Giovane <giovanedaniell@gmail.com>
2022-11-01 14:36:31 -0300

    styles: create Styles for TaskList component

---
+++ src/components/TaskList/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  width: 100%;
+  flex: 1;
+  background: transparent;
+  padding: 20px 30px;
+`
+
+export const Title = styled.Text`
+  font-size: 24px;
+  margin-bottom: 16px;
+  color: #000;
+  font-weight: 700;
+`
