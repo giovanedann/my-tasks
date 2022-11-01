d9bcfcce628e85bee55186d48eb970f51d3bbd02
Giovane <giovanedaniell@gmail.com>
2022-11-01 15:20:38 -0300

    feat: wrap components on Task provider

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {TaskProvider} from './src/contexts/tasks'
 import Home from './src/screens/Home'
 
 const App = () => {
-  return <Home />
+  return (
+    <TaskProvider>
+      <Home />
+    </TaskProvider>
+  )
 }
 
 export default App
